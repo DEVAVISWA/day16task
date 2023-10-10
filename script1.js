a9f58d8eb3492a6b358525463ce8c1c838014bb9
DEVAVISWA G <140805126+DEVAVISWA@users.noreply.github.com>
2023-10-10 14:52:07 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,36 @@
+var countdown = document.createElement('div')
+setTimeout(() => {
+    countdown.innerText = `10`
+}, 1000)
+    setTimeout(() => {
+        countdown.innerText = `9`
+    }, 2000)
+        setTimeout(() => {
+            countdown.innerText = `8`
+        }, 3000)
+                setTimeout(() => {
+                    countdown.innerText = `7`
+                }, 4000)
+                        setTimeout(() => {
+                            countdown.innerText = `6`
+                        }, 5000)
+                                    setTimeout(() => {
+                                        countdown.innerText = `5`
+                                    }, 6000)
+                        setTimeout(() => {
+                            countdown.innerText = `4`
+                        }, 7000)
+                setTimeout(() => {
+                    countdown.innerText = `3`
+                }, 8000)
+            setTimeout(() => {
+                countdown.innerText = `2`
+            }, 9000)
+    setTimeout(() => {
+        countdown.innerText = `1`
+    }, 10000)
+setTimeout(() => {
+    countdown.innerText = `HAPPY INDEPENDANCE DAY`
+}, 11000)
+
+document.body.appendChild(countdown)
